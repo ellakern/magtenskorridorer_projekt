8a7aba493fcba004517594b5111cf826c2e6f54d
nynnetijhuis <nyti0001@stud.ek.dk>
2025-12-15 09:38:38 +0100

    formsnæstenslut

---
+++ JS/forms.js
@@ -0,0 +1,40 @@
+/**** submit */
+
+const form = document.querySelector("form");
+
+const firstNameOutput = document.querySelector("#hundname_output");
+const lastNameOutput = document.querySelector("#alder_output");
+const termsOutput = document.querySelector("#terms_output");
+const fareOutput = document.querySelector("#fare_output");
+const farveOutput = document.querySelector("#farve_output");
+const placeringOutput = document.querySelector("#placering_output");
+
+function cancelPopup(event) {
+  event.preventDefault();
+  form.querySelector(":user-invalid").focus();
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(form);
+
+  const firstName = formData.get("hundname");
+  const lastName = formData.get("alder");
+  const placering = formData.get("placering");
+  const terms = formData.get("accept_terms");
+  const fare = formData.getAll("fare").join(", ");
+  const farve = formData.get("farve");
+
+  firstNameOutput.textContent = firstName;
+  lastNameOutput.textContent = lastName;
+  placeringOutput.textContent = placering;
+  farveOutput.textContent = farve;
+  farveOutput.style.backgroundColor = farve;
+
+  termsOutput.textContent = terms;
+  fareOutput.textContent = fare;
+  form.reset();
+}
+
+form.addEventListener("invalid", cancelPopup, true);
+form.addEventListener("submit", handleSubmit);
